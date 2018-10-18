0714c49b8231ca61acba9ecbd07100a470e21c66
Braulio Camarena <braulio.camarenah@gmail.com>
2018-10-17 18:58:21 -0600

    Fix Sql Query

--- userInsert.php
+++ userInsert.php
@@ -1,13 +1,14 @@
 <?php
     include("connection.php");
 
-    $name = $_POST("addName");
-    $lastName= $_POST("addLastName");
-    $nickname=$_POST("addNicname");
-    $email= $_POST("addEmail");
-    $password =$_POST("addPassword");
+    $name = $_POST["addName"];
+    $lastName= $_POST["addLastName"];
+    $nickname=$_POST["addNickname"];
+    $email= $_POST["addEmail"];
+    $password =$_POST["addPassword"];
 
-    $sql="INSERT INTO Usuarios (nombre, apellido, nickname,email, contraseña) VALUES ('".$name."','".$lastName."','".$nickname."','".$email."','".$password."')";
+    $sql="INSERT INTO Usuarios (nombre, apellido, nickname,email, contraseña) 
+          VALUES ('".$name."','".$lastName."','".$nickname."','".$email."','".$password."')";
 
 
     $result= mysqli_query($connect, $sql);
