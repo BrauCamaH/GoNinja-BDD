5d19749e4c997644c5757928990e60cb09589131
Braulio Camarena <braulio.camarenah@gmail.com>
2018-10-27 18:54:37 -0600

    Fix useSelect

--- connection.php
+++ connection.php
@@ -5,10 +5,4 @@
     $db="GoNinja";
 
     $connect = new mysqli($server,$username,$password,$db);
-
-    // Check connection
-    if (mysqli_connect_errno())
-    {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }else  echo "Connection Success" ;
 ?>
--- userSelect.php
+++ userSelect.php
@@ -1,7 +1,25 @@
 <?php
     include("connection.php");
 
-    $sql = "SELECT u.nombre, u.contraseña FROM Usuarios u";
+    $email= $_POST["email"];
+    $password= $_POST["password"];
 
-    $result= mysqli_query($connect, $sql);
+    $sql = "SELECT u.email, u.contraseña FROM Usuarios u WHERE email ='". $email . "';";
+
+
+    $namecheck = mysqli_query($connect, $sql) or die("query failed");
+    if (mysqli_num_rows($namecheck) !=1 ) {
+        echo "No user, or more than one ";
+        exit();
+    }
+
+    $logininfo = mysqli_fetch_assoc($namecheck);
+    $query_password = $logininfo["contraseña"];
+
+    if($query_password != $password){
+        echo "Incorrect password";
+        exit();
+    }
+
+    echo '0';
 ?>
