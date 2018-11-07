ce8902e6c156affd49b5d32ae1aa9f259afb2eca
Braulio Camarena <braulio.camarenah@gmail.com>
2018-11-06 18:26:23 -0600

    Add id verification

---
+++ levelSelect.php
@@ -0,0 +1,16 @@
+<?php
+    include("connection.php");
+    $id= $_POST["id"];
+    $sql = "SELECT l.nombre, l.puntos , l.intentos , l.completado FROM Niveles u  WHERE id ='". $id . "';";
+
+    $result= mysqli_query($connect, $sql);
+
+    if(mysqli_num_rows($result>0)){
+        while ($row = mysqli_fetch_assoc($result)){
+            echo $row["nombre"] . "\t". $row["puntos"] . "\t". $row["intentos"] . "\t". $row["completado"];
+        }
+    }else {
+        echo "0";//means that there not data yet 
+    }
+
+?>
