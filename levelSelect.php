2dcab9f60beaf6b663311c3128415d1137c20549
Braulio Camarena <braulio.camarenah@gmail.com>
2018-11-07 12:12:04 -0600

    Fix bugs

--- levelSelect.php
+++ levelSelect.php
@@ -1,16 +1,16 @@
 <?php
     include("connection.php");
     $id= $_POST["id"];
-    $sql = "SELECT l.nombre, l.puntos , l.intentos , l.completado FROM Niveles l  WHERE id ='". $id . "';";
-
+    $sql = "SELECT * FROM Niveles l  
+            INNER JOIN Usuarios s
+            ON l.id = s.id
+            WHERE l.id ='". $id . "';";
+ 
     $result= mysqli_query($connect, $sql);
 
-    if(mysqli_num_rows($result>0)){
+    if(mysqli_num_rows($result)>0){
         while ($row = mysqli_fetch_assoc($result)){
-            echo $row["nombre"] . "\t". $row["puntos"] . "\t". $row["intentos"] . "\t". $row["completado"];
+            echo  $row["nombre"] . "\t". $row["puntos"] . "\t". $row["intentos"] . "\t". $row["completado"];
         }
-    }else {
-        echo "0";//means that there not data yet 
     }
-
 ?>
