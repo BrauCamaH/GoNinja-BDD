fc8b0a6705855a63dad11798a75b992cc959b8d9
Braulio Camarena <braulio.camarenah@gmail.com>
2018-11-08 15:12:46 -0600

    Fix sql query

--- levelUpdate.php
+++ levelUpdate.php
@@ -1,13 +1,12 @@
 <?php
     include("connection.php");
     $id= $_POST["id"];
-    $name = $_POST["name"];
     $score = $_POST["score"];
     $attemps =  $_POST["attemps"];
     $isComplete =  $_POST["isComplete"];
 
     $sql = "UPDATE Niveles l 
-            SET  l.nombre = $nombre , l.puntos= $score , l.intentos = $attemps , l.completado = $isComplete
+            SET   l.puntos= $score , l.intentos = $attemps , l.completado = $isComplete
             WHERE id ='". $id . "';";
 
     $result= mysqli_query($connect, $sql);
